2dfdeec52858d0eda4051cedc009f6a913a93161
Mohamed Abdallah <128813258+Mohamedkskdhkshx@users.noreply.github.com>
2024-08-18 10:23:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let img = document.getElementById("cl")
+let parent = document.getElementById("parent")
+let baseImg  =document.getElementById("base-img")
+
+function change(phone){
+  baseImg.src = phone
+}
+
+
+function bacChnge(color){
+   parent.style.background = color
+}
